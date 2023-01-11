475a80fa1e5410e751c4fe890aac51cc78eb440c
fdlrahman <jedacoding@gmail.com>
2023-01-11 16:41:32 +0800

    finalize

--- src/components/_child/related.js
+++ src/components/_child/related.js
@@ -1,47 +1,53 @@
 import React from "react";
 import Link from "next/link";
 import Author from "./author";
+import fetcher from "../../lib/fetcher";
+import Spinner from "./spinner";
+import Error from "./error";
+import Image from "next/image";
 
 function Related() {
+  const { data, isLoading, isError } = fetcher("trending");
+
+  if (isLoading) return <Spinner></Spinner>;
+  if (isError) return <Error></Error>;
+
   return (
     <div>
       <h1 className="text-xl font-bold">Related</h1>
 
       <div className="posts py-4 flex flex-col gap-3">
-        <Post />
-        <Post />
+        {data.map((value, index) => (
+          <Post data={value} key={index}></Post>
+        ))}
       </div>
     </div>
   );
 }
 
-function Post() {
-  const bg = {
-    background: "url(/images/articles/img2.jpg) no-repeat",
-    backgroundSize: "cover",
-    backgroundPosition: "center center",
-  };
-
+function Post({ data }) {
   return (
     <div className="flex gap-3 my-4">
       <div className="image">
-        <div className="bg-gray-500 w-40 h-32" style={bg}></div>
+        <Link href={`/posts/${data.id}`}>
+          <Image src={data.img} width={200} height={200}></Image>
+        </Link>
       </div>
       <div className="info">
         <div className="cat text-sm">
-          <Link href={"/"} className="text-sky-600">
-            Business, Travel
+          <Link href={`/posts/${data.id}`} className="text-sky-600">
+            {data.category}
           </Link>
-          <Link href={"/"} className="text-gray-800">
-            - July 3, 2022
+          <Link href={`/posts/${data.id}`} className="text-gray-800">
+            - {data.published}
           </Link>
         </div>
         <div className="title">
-          <Link href={"/"} className="font-bold text-1xl hover:text-gray-700">
-            Your most unhappy customers are your greatest source of learning
+          <Link href={`/posts/${data.id}`} className="font-bold text-1xl hover:text-gray-700">
+            {data.title}
           </Link>
         </div>
-        <Author />
+        {data.author ? <Author data={data.author}></Author> : <></>}
       </div>
     </div>
   );
